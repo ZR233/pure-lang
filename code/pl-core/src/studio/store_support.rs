cb83586bd32c35a90d223c4cd9a5fdb17799d156
周睿 <zrufo747@outlook.com>
2026-07-01 22:06:25 +0800

    Add migration test for legacy agent timeline pruning

--- code/pl-core/src/studio/store_support.rs
+++ code/pl-core/src/studio/store_support.rs
@@ -31,6 +31,7 @@ const MIGRATIONS: &[&str] = &[
     include_str!("../../migrations/0020_message_part_revision.sql"),
     include_str!("../../migrations/0021_message_part_activity_group.sql"),
     include_str!("../../migrations/0022_drop_legacy_agent_and_handoff_tables.sql"),
+    include_str!("../../migrations/0023_prune_legacy_agent_timeline_events.sql"),
 ];
 
 pub(super) async fn insert_message_with_tx(
